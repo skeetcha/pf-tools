60815fa894ec55e48021d26190c1b08ebdc6c845
Daniel Unterholzner <pianojammer@comcast.net>
2019-08-11 19:15:11 -0700

    Dice rolling now works, Macros work, commands work. Need to add drop lowest and drop highest functionality.

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,3 +1,5 @@
+$("body").append('<div class="rollbox-min"><span class="glyphicon glyphicon-chevron-up"></span></div><div class="rollbox hidden"><div class="head-roll"><span class="hdr-roll">Dice Roller</span><span class="delete-icon glyphicon glyphicon-remove"></span></div><div class="out-roll"></div><input class="ipt-roll" autocomplete="off" spellcheck="false" placeholder="3d6-1 or &quot;/help&quot;"></div>');
+
 $("span.glyphicon-chevron-up").on('click', (event) => {
 	$(".rollbox-min").addClass("hidden");
 	$(".rollbox").removeClass("hidden");
@@ -8,25 +10,208 @@ $("span.glyphicon-remove").on('click', (event) => {
 	$(".rollbox-min").removeClass("hidden");
 });
 
-$("input.ipt-roll").keypress(function(e) {
-	if (e.which == 13) {
-		var diceCheck = /([1-9]\d*)?d([1-9]\d*)(\s?([+-])\s?(\d+))?/i;
-		var result = $("input.ipt-roll").val().match(diceCheck);
+function strip(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+}
 
-		if ((result == null) && ($("input.ipt-roll").val() == "/help")) {
-			/* Append Drop highest (`2d4dh1`) and lowest (`4d6dl1`) are supported.
-Up and down arrow keys cycle input history.
-Anything before a colon is treated as a label (`Fireball: 8d6`)
-Use `/macro list` to list saved macros.
-Use `/macro add myName 1d2+3` to add (or update) a macro. Macro names should not contain spaces or hashes.
-Use `/macro remove myName` to remove a macro.
-Use `#myName` to roll a macro.
-Use `/clear` to clear the roller.*/
-			$("div.out-roll").append("<p>Append Drop highest (<code>2d4dh1</code>) and lowest (<code>4d6dl1</code>) are supported.<br />Up and down arrow keys cycle input history.<br />Anything before a colon is treated as a label (<code>Fireball: 8d6</code>)<br />Use <code>/macro list</code> to list saved macros.<br />Use <code>macro add myName 1d2+3</code> to add (or update) a macro. Macro names should not contain spaces or hashes.<br />Use <code>/macro remove myName</code> to remove a macro.<br />Use <code>#myName</code> to roll a macro.<br />Use <code>/clear</code> to clear the roller.</p>");
-		} else if ((result == null) && ($("input.ipt-roll").val() == "/clear")) {
-			$("div.out-roll").text("");
+function parseCommand(cmd) {
+	if (cmd == "/help") {
+		$("div.out-roll").append("Drop highest (<code>2d4dh1</code>) and lowest (<code>4d6dl1</code>) are supported.<br />Up and down arrow keys cycle input history.<br />Anything before a colon is treated as a label (<code>Fireball: 8d6</code>)<br />Use <code>/macro list</code> to list saved macros.<br />Use <code>/macro add myName</code> to add (or update) a macro. Macro names should not contain spaces or hashes.<br />Use <code>/macro remove myName</code> to remove a macro.<br />Use <code>#myName</code> to roll a macro.<br />Use <code>/clear</code> to clear the roller.<br /><br />");
+	} else if (cmd == "/clear") {
+		$("div.out-roll").text("");
+	} else if (cmd.startsWith("/macro")) {
+		var args = cmd.split(" ");
+
+		if (args[1] == "add") {
+			var macro = "";
+
+			for (var i = 3; i < args.length; i++) {
+				macro += args[i];
+				macro += " ";
+			}
+
+			macro = strip(macro);
+
+			if (localStorage.getItem("macros") == null) {
+				localStorage.setItem("macros", '{"macros":[{"' + args[2] + '": "' + macro + '"}]}');
+			} else {
+				var macros = JSON.parse(localStorage.getItem("macros"));
+
+				for (var i = 0; i < macros.macros.length; i++) {
+					if (Object.keys(macros.macros[i]).indexOf(args[2]) >= 0) {
+						macros.macros[i][args[2]] = macro;
+					}
+				}
+
+				localStorage.setItem("macros", JSON.stringify(macros));
+			}
+
+			$("div.out-roll").append("Successfully added " + args[2] + " macro.<br /><br />");
+		} else if (args[1] == "remove") {
+			if (localStorage.getItem("macros") == null) {
+				$("div.out-roll").append("That macro does not exist.<br /><br />");
+			} else {
+				var macros = JSON.parse(localStorage.getItem("macros"));
+				var i;
+
+				for (i = 0; i < macros.macros.length; i++) {
+					if (Object.keys(macros.macros[i]).indexOf(args[2]) >= 0) {
+						break;
+					}
+				}
+
+				delete macros.macros[i];
+				localStorage.setItem("macros", JSON.stringify(macros));
+				$("div.out-roll").append("Succesfully removed " + args[2] + " macro.<br /><br />");
+			}
+		} else if (args[1] == "list") {
+			if (localStorage.getItem("macroList") == null) {
+				$("div.out-roll").append("List of macros: <br /><br />");
+			} else {
+				var macros = JSON.parse(localStorage.getItem("macros"));
+
+				$("div.out-roll").append("List of macros: ");
+
+				for (var i = 0; i < macros.macros.length; i++) {
+					$("div.out-roll").append(Object.keys(macros.macros[i])[0]);
+				}
+
+				$("div.out-roll").append("<br /><br />");
+			}
+		}
+	}
+}
+
+function rollDie(dieType) {
+	return Math.floor(Math.random() * (dieType - 1)) + 1;
+}
+
+function rollDice(dieAmount, dieType) {
+	var data = {"sum": 0, "rolls": []}
+
+	for (var i = 0; i < dieAmount; i++) {
+		var roll = rollDie(dieType);
+		data["sum"] += roll;
+		data["rolls"].push(roll);
+	}
+
+	return data;
+}
+
+function processInput(input) {
+	var diceCheck = /([1-9]\d*)?d([1-9]\d*)(\s?([+-])\s?(\d+))?/i;
+	var result = input.match(diceCheck);
+	var isCmd = input.startsWith("/");
+	var isMacro = input.startsWith("#");
+
+	if (isCmd) {
+		parseCommand(input);
+	} else if (isMacro) {
+		parseMacro(input.replace("#", ""));
+	} else if (!isCmd && !isMacro && result) {
+		var rollData = rollDice(Number(result[1]), Number(result[2]));
+		var critSuccess = false;
+		var critFail = false;
+
+		for (var i = 0; i < rollData.rolls.length; i++) {
+			if (rollData.rolls[i] == Number(result[2])) {
+				critSuccess = true;
+			} else if (rollData.rolls[i] == 1) {
+				critFail = true;
+			}
 		}
 
+		if (result[3] == null) {
+			if (critSuccess && !critFail) {
+				$("div.out-roll").append('Result: <span class="roll-crit">' + String(rollData.sum) + '</span> (');
+			} else if (critFail && !critSuccess) {
+				$("div.out-roll").append('Result: <span class="roll-fail">' + String(rollData.sum) + '</span> (');
+			} else if (critFail && critSuccess) {
+				$("div.out-roll").append('Result: <span class="roll-mixed">' + String(rollData.sum) + '</span> (');
+			} else {
+				$("div.out-roll").append("Result: " + String(rollData.sum) + " (");
+			}
+		} else {
+			if (critSuccess && !critFail) {
+				$("div.out-roll").append('Result: <span class="roll-crit">' + String(rollData.sum + Number(result[3])) + '</span> (');
+			} else if (critFail && !critSuccess) {
+				$("div.out-roll").append('Result: <span class="roll-fail">' + String(rollData.sum + Number(result[3])) + '</span> (');
+			} else if (critFail && critSuccess) {
+				$("div.out-roll").append('Result: <span class="roll-mixed">' + String(rollData.sum + Number(result[3])) + '</span> (');
+			} else {
+				$("div.out-roll").append("Result: " + String(rollData.sum + Number(result[3])) + " (");
+			}
+		}
+
+		if (rollData.rolls.length == 1) {
+			if (result[3] == null) {
+				if (rollData.rolls[0] == Number(result[2])) {
+					$("div.out-roll").append('<span class="roll-crit">' + result[2] + '</span>)<br /><br />');
+				} else if (rollData.rolls[0] == 1) {
+					$("div.out-roll").append('<span class="roll-fail">1</span>)<br /><br />');
+				} else {
+					$("div.out-roll").append(String(rollData.rolls[0]) + ')<br /><br />');
+				}
+			} else {
+				if (rollData.rolls[0] == Number(result[2])) {
+					$("div.out-roll").append('<span class="roll-crit">' + result[2] + '</span> ' + result[4] + ' ' + result[5] + ')<br /><br />');
+				} else if (rollData.rolls[0] == 1) {
+					$("div.out-roll").append('<span class="roll-fail">1</span> ' + result[4] + ' ' + result[5] + ')<br /><br />');
+				} else {
+					$("div.out-roll").append(String(rollData.rolls[0]) + ' ' + result[4] + ' ' + result[5] + ')<br /><br />');
+				}
+			}
+		} else {
+			for (var i = 0; i < rollData.rolls.length - 1; i++) {
+				if (rollData.rolls[i] == Number(result[2])) {
+					$("div.out-roll").append('<span class="roll-crit">' + String(rollData.rolls[i]) + '</span> + ');
+				} else if (rollData.rolls[i] == 1) {
+					$("div.out-roll").append('<span class="roll-fail">1</span> + ');
+				} else {
+					$("div.out-roll").append(String(rollData.rolls[i]) + " + ");
+				}
+			}
+
+			if (result[3] == null) {
+				if (rollData.rolls[rollData.rolls.length - 1] == Number(result[2])) {
+					$("div.out-roll").append('<span class="roll-crit">' + result[2] + '</span>)<br /><br />');
+				} else if (rollData.rolls[rollData.rolls.length - 1] == 1) {
+					$("div.out-roll").append('<span class="roll-fail">1</span>)<br /><br />');
+				} else {
+					$("div.out-roll").append(String(rollData.rolls[rollData.rolls.length - 1]) + ")<br /><br />");
+				}
+			} else {
+				if (rollData.rolls[rollData.rolls.length - 1] == Number(result[2])) {
+					$("div.out-roll").append('<span class="roll-crit">' + result[2] + '</span> ' + result[4] + ' ' + result[5] + ')<br /><br />');
+				} else if (rollData.rolls[rollData.rolls.length - 1] == 1) {
+					$("div.out-roll").append('<span class="roll-fail">1</span> ' + result[4] + ' ' + result[5] + ')<br /><br />');
+				} else {
+					$("div.out-roll").append(String(rollData.rolls[rollData.rolls.length - 1]) + " " + result[4] + " " + result[5] + ")<br /><br />");
+				}
+			}
+		}
+	}
+	
+	$("div.out-roll").scrollTop($("div.out-roll")[0].scrollHeight);
+}
+
+function parseMacro(macro) {
+	if (localStorage.getItem("macros") == null) {
+		$("div.out-roll").append("That macro does not exist.<br /><br />");
+	} else {
+		var macros = JSON.parse(localStorage.getItem("macros"));
+
+		for (var i = 0; i < macros.macros.length; i++) {
+			if (Object.keys(macros.macros[i]).indexOf(macro) >= 0) {
+				processInput(macros.macros[i][macro]);
+			}
+		}
+	}
+}
+
+$("input.ipt-roll").keypress(function(e) {
+	if (e.which == 13) {
+		processInput($("input.ipt-roll").val());
 		$("input.ipt-roll").val("");
 	}
 });
